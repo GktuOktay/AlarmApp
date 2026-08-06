1c99e3c1be8e8252e2f44fc1f99164d08545762a
Göktuğ OKTAY <GktuOktay@users.noreply.github.com>
2026-08-06 21:11:26 +0300

    düzeltme: AlarmSchedule tanımlarını CreateAlarmGroup dosyasına taşı
    Xcode indeksinin yeni dosyayı kaçırması nedeniyle kapsam hatalarını giderir.

--- AlarmAppCore/Sources/AlarmAppCore/Domain/CreateAlarmGroup.swift
+++ AlarmAppCore/Sources/AlarmAppCore/Domain/CreateAlarmGroup.swift
@@ -45,6 +45,25 @@ public enum CreateAlarmGroupError: Error, Equatable, Sendable {
     case generation(AlarmInstanceGenerationError)
 }
 
+public enum AlarmFireDate {
+    /// Combines a day (any time) with a clock time in the given calendar.
+    public static func make(day: Date, time: ClockTime, calendar: Calendar = .current) -> Date? {
+        let start = calendar.startOfDay(for: day)
+        return calendar.date(bySettingHour: time.hour, minute: time.minute, second: 0, of: start)
+    }
+}
+
+public struct AlarmSchedule: Sendable, Equatable, Identifiable {
+    public var id: UUID { instanceId }
+    public let instanceId: UUID
+    public let fireDate: Date
+
+    public init(instanceId: UUID, fireDate: Date) {
+        self.instanceId = instanceId
+        self.fireDate = fireDate
+    }
+}
+
 public struct CreateAlarmGroupResult: Sendable {
     public let groupId: UUID
     public let instanceCount: Int
